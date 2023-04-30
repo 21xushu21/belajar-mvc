9719c33f8b977dfdabf6e9ebb6cc3a9052bddfa0
Xushu <uchiwa.syahada@gmail.com>
2023-04-30 21:44:40 +0700

    view v3

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,9 +5,15 @@ class About extends Controller {
         $data['nama'] =$nama;
         $data['pekerjaan'] =$pekerjaan;
         $data['umur'] =$umur;
+        $data['judul']= 'about me';
+        $this->view('templates/header', $data);
         $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page() {
+        $data['judul']= 'page';
+        $this->view('templates/header', $data);
         $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
