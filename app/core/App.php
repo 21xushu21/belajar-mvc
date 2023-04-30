3b3718befa6d8d25d61202943c55deac4226e5a0
Xushu <uchiwa.syahada@gmail.com>
2023-04-30 20:40:37 +0700

    conttoler v4

--- app/core/App.php
+++ app/core/App.php
@@ -25,6 +25,8 @@ class App {
         if( !empty($url)) {
             $this->params = array_values($url);
         }
+        // jalankan controller & method, serta kirimkan params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
 
     }
     public function parseURL() {
