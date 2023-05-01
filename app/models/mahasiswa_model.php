8aa59bea1e33b9b8ae573b8cdc3464e261350276
Xushu <uchiwa.syahada@gmail.com>
2023-05-01 14:05:16 +0700

    database v4

--- app/models/mahasiswa_model.php
+++ app/models/mahasiswa_model.php
@@ -1,23 +1,20 @@
 <?php 
 class Mahasiswa_model {
-    private $mhs = [
-        [
-            "nama" => "madon",
-            "alamat" => "jlmndsf",
-            "email" => "gadfh@daj.com",
-        ],
-        [
-            "nama" => "agus",
-            "alamat" => "hgjfjyt",
-            "email" => "gaedasf@daj.com",
-        ],        
-        [
-            "nama" => "yaya",
-            "alamat" => "asdfasdf",
-            "email" => "asdfasd@daj.com",
-        ],
-        ];
+    private $dbh; //database handler
+    private $stmt;
+    public function __construct() {
+        // data source name
+        $dsn = 'msyql:host=localhost;dbname=phpmvc';
+        // pengecekan koneksi
+        try {
+            $this->dbh =new PDO ($dsn,'root','');
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
     public function getAllMahasiswa() {
-        return $this->mhs;
+        $this->stmt = $this->dbh->prepare('select * FROM mahasiswa');
+        $this->stmt->execute(); //menjalankan qeerynya
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
